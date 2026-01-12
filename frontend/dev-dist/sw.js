8ad3a3b605456185a70c20de770ab8417344451e
Insofanh <anhha2k2@gmail.com>
2026-01-12 09:35:37 +0700

    update php version

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,12 +85,12 @@ define(['./workbox-a1974cce'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.0gqifto0j3g"
+    "revision": "0.lqjituuell8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
     allowlist: [/^\/$/],
-    denylist: [/^\/api/]
+    denylist: [/^\/api/, /^\/admin/]
   }));
   workbox.registerRoute(/^https:\/\/fonts\.googleapis\.com\/.*/i, new workbox.CacheFirst({
     "cacheName": "google-fonts-cache",
